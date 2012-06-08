d2cc1d6b0ccce0b91367cd51e1f9e7ec9bb1579d
Alan van Dam <alan@vandamnet.nl>
2012-06-08 17:18:23 +0200

    canvas installed until bundle install, added mysql, rvm

--- cookbooks/canvas/recipes/default.rb
+++ cookbooks/canvas/recipes/default.rb
@@ -1,10 +1,24 @@
 %w{"zlib1g-dev" "libxml2-dev" "libxslt-dev" "libsqlite3-dev" "libhttpclient-ruby" "imagemagick"}.each { |p|
     package p do
-        action [:install]
+        action :install
     end
 }
 
 execute "clone-canvas-repo" do
     command "git clone https://github.com/instructure/canvas-lms.git /opt/canvas"
     creates "/opt/canvas"
+end
+
+cookbook_file "/opt/canvas/.rvmrc" do
+    source "rvmrc"
+    mode "0644"
+end
+
+gem_package "bundler" do
+    action :install
+end
+
+execute "bundle-install" do
+    cwd "/opt/canvas"
+    command "bundle install --without postgres"
 end
